df7ab6a77582ebaaa16685c3f9c4a2bcf40e236d
Martin Böhm <martin.bohm@kubuntu.org>
2007-08-09 14:52:08 +0200

    Added the setup.py file, removed the debian/install file. Time to show the branch to the world.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from distutils.core import setup
+import glob
+setup(
+    name = 'onboard',
+    author = 'Chris Jones',
+    author_email = 'cej105@soton.ac.uk',
+    maintainer = 'Ubuntu Core Developers',
+    maintainer_email = 'ubuntu-devel-discuss@lists.ubuntu.com',
+    url = 'http://launchpad.net/onboard/',
+    license = 'gpl',
+    description = 'Simple On-screen Keyboard',
+    packages = ['Onboard'],
+    data_files = [('share/onboard', glob.glob('data/*'))],
+    scripts = ['onboard', 'onboard-settings']
+)
+
