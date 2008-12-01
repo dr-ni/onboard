46728514ea8395e3a2300f7b8af1afcce592f976
Francesco Fumanti <francesco.fumanti@gmx.net>
2008-12-01 15:46:41 +0100

    Fix: gconf key name for the trayicon.

--- Onboard/OnboardGtk.py
+++ Onboard/OnboardGtk.py
@@ -105,7 +105,7 @@ class OnboardGtk(object):
         self.gconfClient.notify_add("/apps/onboard/snippets",self.do_change_macros)
         self.gconfClient.notify_add("/apps/onboard/scanning_interval", self.do_change_scanningInterval)
         self.gconfClient.notify_add("/apps/onboard/enable_scanning", self.do_change_scanning)
-        self.gconfClient.notify_add("/apps/onboard/trayicon", self.do_set_trayicon)
+        self.gconfClient.notify_add("/apps/onboard/use_trayicon", self.do_set_trayicon)
         
         if self.gconfClient.get_bool("/apps/onboard/use_trayicon"):
             self.hide_status_icon()
