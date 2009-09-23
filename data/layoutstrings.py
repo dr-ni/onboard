663cda61795f77c1260f25d0c65b0ce5efdc9541
Chris Jones <tortoise@tortuga>
2009-09-23 19:33:32 +0100

    translate all non-character keyboard labels

--- data/layoutstrings.py
+++ data/layoutstrings.py
@@ -1,8 +1,50 @@
 #!/usr/bin/python
 from gettext import gettext as _
 
-TRANSLATABLE_LAYOUT_STRINGS = [_("Switch\nButtons"),
-                               _("Settings")]
+TRANSLATABLE_LAYOUT_STRINGS = [
+    _("Switch\nButtons"),
+    _("Settings"),
+    # The tab key on a computer keyboard
+    _("Tab"),
+    # The caps lock key on a computer keyboard
+    _("CAPS"),
+    # The control key on a computer keyboard
+    _("Ctrl"),
+    # The windows or meta key on a computer keyboard
+    _("Win"),
+    # The alt key on a computer keyboard
+    _("Alt"),
+    # The alt gr key on a computer keyboard
+    _("Alt Gr"),
+    # The return key on a computer keyboard
+    _("Return"),
+    # The menu key on a computer keyboard
+    _("Menu"),
+    # The insert key on a computer keyboard
+    _("Ins"),
+    # The home key on a computer keyboard
+    _("Hm"),
+    # The del key on a computer keyboard
+    _("Del"),
+    # The end key on a computer keyboard
+    _("End"),
+    # The page up key on a computer keyboard
+    _("Pg\nUp"),
+    # The page down key on a computer keyboard
+    _("Pg\nDn"),
+    # The num lock key on a computer keyboard
+    _("Nm\nLk"),
+    # The enter key on a computer keyboard
+    _("Ent"),
+    # The ecape key on a computer keyboard
+    _("ESC"),
+    # The print screen key on a computer keyboard
+    _("Prnt"),
+    # The scroll lock key on a computer keyboard
+    _("Scroll"),
+    # The pause key on a computer keyboard
+    _("Pause"),
+]
 
 raise Exception("This module should not be executed.  It should only be"
         " parsed by i18n tools such as intltool.")
