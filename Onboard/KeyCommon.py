674ad43dee89a3697c6f5ad3a033eb3864965ea0
marmuta <marmvta@gmail.com>
2012-02-14 08:20:12 +0100

    Fix dish key background shifting with ModelM key action. No more gaps opening up when pressing keys.

--- Onboard/KeyCommon.py
+++ Onboard/KeyCommon.py
@@ -212,16 +212,6 @@ class RectKeyCommon(KeyCommon):
         """ Get bounding box of the key at 100% size in logical coordinates """
         rect = LayoutItem.get_rect(self)
 
-        # fake physical key action
-        if self.pressed:
-            key_style = config.theme_settings.key_style
-            if key_style == "dish":
-                k = 0.45
-                rect.x += k
-                rect.y += 2 * k
-                rect.w - 2 * k
-                rect.h - k
-
         return rect
 
     def get_canvas_fullsize_rect(self):
@@ -231,6 +221,16 @@ class RectKeyCommon(KeyCommon):
     def get_rect(self):
         """ Get bounding box in logical coordinates """
         rect = self.get_fullsize_rect()
+
+        # fake physical key action
+        if self.pressed:
+            key_style = config.theme_settings.key_style
+            if key_style == "dish":
+                k = 0.45
+                rect.x += k
+                rect.y += 2 * k
+                rect.w - 2 * k
+                rect.h - k
 
         size = config.theme_settings.key_size / 100.0
         border = rect.h * (1.0 - size) / 2.0
