3ba71d5655a8cbfe1cc17f708aa6b8b1809cbca8
marmuta <marmvta@gmail.com>
2012-03-18 17:43:55 +0100

    Zombie process protection: Revert sokSettings.py to commit 759. Commit 760 makes onboard hang on exit until all preference windows are closed. This essentially daemonizes onboard-settings, making init the owner. I don't have a better solution right now.

--- scripts/sokSettings.py
+++ scripts/sokSettings.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python
 import os
 import sys
-import subprocess
 
 def run():
+    # double fork to prevent zombies
     pid = os.fork() 
-    if pid == 0:
-        subprocess.call(("python", "-cfrom Onboard.settings import Settings\ns = Settings(False)"))
-        os._exit(os.EX_OK)
+    if pid > 0:
+        # grand parent
+        return
+
+    # first child
+    pid = os.fork() 
+    if pid > 0:
+        # parent
+        sys.exit(0)
+
+    # second child
+    os.execvp("python", ("python", "-cfrom Onboard.settings import Settings\ns = Settings(False)"))
 
 
 
