50d520821980630315c1ef89d6a7d095c8ff1966
marmuta <marmvta@gmail.com>
2012-06-21 10:20:41 +0200

    Merge word-completion branch 266: Remove build directory from python path. Python3 wants absolute import paths and osk has to be run directly from the source directory.

--- Onboard/__init__.py
+++ Onboard/__init__.py
@@ -2,19 +2,5 @@
 
 from __future__ import division, print_function, unicode_literals
 
-try:
-    from . import osk
-except ImportError:
-    # We might be running from source, so try again with 
-    # the build directory in the path
-    import sys, os, glob
-    from os.path import dirname, abspath, join
-    path = dirname(dirname(abspath(__file__)))
-    pattern = join(path, 'build', 
-                         'lib*{}.*'.format(sys.version_info.major),
-                         'Onboard')
-    paths = glob.glob(pattern)
-    if paths:
-        sys.path.append(paths[0])
-    print("running from source; looking for osk in " + str(paths))
+
 
