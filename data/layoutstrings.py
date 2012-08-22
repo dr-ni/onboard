2bc2e2abdffe68741dff032befcfd5a162f150d7
marmuta <marmvta@gmail.com>
2012-08-22 09:05:39 +0200

    Add comments for translators to hopefully keep key labels short. The Catalan translation has a huge string for the ESC key that makes a couple of labels unreadable.

--- data/layoutstrings.py
+++ data/layoutstrings.py
@@ -14,41 +14,62 @@ from gettext import gettext as _
 TRANSLATABLE_LAYOUT_STRINGS = [
     _("Activate Hover Click"),
     _("Alphanumeric keys"),
+    # translators: very short label of the left Alt key
     _("Alt"),
+    # translators: very short label of the Alt Gr key key
     _("Alt Gr"),
+    # translators: very short label of the CAPS LOCK key
     _("CAPS"),
+    # translators: very short label of the Ctrl key
     _("Ctrl"),
+    # translators: very short label of the DELETE key
     _("Del"),
     _("Double click"),
     _("Drag click"),
+    # translators: very short label of the numpad END key
     _("End"),
+    # translators: very short label of the numpad ENTER key
     _("Ent"),
+    # translators: very short label of the ESCAPE key
     _("Esc"),
     _("Function keys"),
     _("Number block and function keys"),
     _("Hide Onboard"),
+    # translators: very short label of the HOME key
     _("Hm"),
+    # translators: very short label of the INSERT key
     _("Ins"),
     _("Main keyboard"),
+    # translators: very short label of the Menu key
     _("Menu"),
     _("Middle click"),
     _("Move Onboard"),
+    # translators: very short label of the NUMLOCK key
     _("Nm&#10;Lk"),
     _("Number block and snippets"),
+    # translators: very short label of the PAUSE key
     _("Pause"),
+    # translators: very short label of the PAGE DOWN key
     _("Pg&#10;Dn"),
+    # translators: very short label of the PAGE UP key
     _("Pg&#10;Up"),
+    # translators: very short label of the Preferences button
     _("Preferences"),
+    # translators: very short label of the PRINT key
     _("Prnt"),
+    # translators: very short label of the Quit button
     _("Quit"),
+    # translators: very short label of the RETURN key
     _("Return"),
     _("Right click"),
+    # translators: very short label of the SCROLL key
     _("Scroll"),
-    _("Settings"),
     _("Snippets"),
     _("Space"),
     _("Toggle click helpers"),
+    # translators: very short label of the TAB key
     _("Tab"),
+    # translators: very short label of the default SUPER key
     _("Win"),
 ]
 
